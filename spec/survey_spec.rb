6d8bbf319ea7158d76dd97891ad4342f414b6488
Andrew M Westling & Eric Johnson <student@epicodus.com>
2014-08-14 10:19:54 -0700

    Add question to survey association"

--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -5,9 +5,7 @@ describe 'Survey' do
 
   it 'adds a question to a survey' do
     test_survey = Survey.create({name: "Food"})
-    test_question = Question.create({text: "What is your favorite type of apple?"})
-    Survey.joins(:questions)
-    test_question.assign_to(test_survey.id)
+    test_question = Question.create({text: "What is your favorite type of apple?", survey_id: test_survey.id.to_i})
     expect(test_survey.questions).to eq [test_question]
   end
 
